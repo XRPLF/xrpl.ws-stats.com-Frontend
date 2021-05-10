2257dbbb34913e11b1bbb512c8e0412bd2e30478
Robert Swarthout <rswarthout@gmail.com>
2021-05-10 10:30:06 -0400

    Adjusting the width to accommodate a larger XRP total.

--- src/components/ClusterStats.js
+++ src/components/ClusterStats.js
@@ -297,34 +297,32 @@ export default class SimpleNodeList extends React.Component {
                     The scam transactions listed below are transactions prevented starting on { this.state.preventedScam.since }.
                     Scam account addresses are provided by <a href={ this.state.preventedScam.scamAccountDataSource.url } className="underline">{ this.state.preventedScam.scamAccountDataSource.name }</a>.
                 </div>
-                <div className="max-w-5xl mx-auto">
-                    <dl className="rounded-lg bg-white shadow grid grid-cols-1 md:grid-cols-3 mt-6">
-                        <div className="flex flex-col border-b border-gray-100 p-6 text-center border-0 border-r">
-                            <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                                Scam Accounts
-                            </dt>
-                            <dd className="order-1 text-5xl font-extrabold text-xrpl-ws-blue">
-                                { Object.keys(this.state.preventedScam.scams).length.toLocaleString() }
-                            </dd>
-                        </div>
-                        <div className="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                            <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                                Total Transactions
-                            </dt>
-                            <dd className="order-1 text-5xl font-extrabold text-xrpl-ws-blue">
-                                { this.state.preventedScam.count.toLocaleString() }
-                            </dd>
-                        </div>
-                        <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
-                            <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                                Total XRP
-                            </dt>
-                            <dd className="order-1 text-5xl font-extrabold text-xrpl-ws-blue">
-                                { this.state.preventedScam.xrp.toLocaleString() }
-                            </dd>
-                        </div>
-                    </dl>
-                </div>
+                <dl className="rounded-lg bg-white shadow grid grid-cols-1 md:grid-cols-3 mt-6">
+                    <div className="flex flex-col border-b border-gray-100 p-6 text-center border-0 border-r">
+                        <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
+                            Scam Accounts
+                        </dt>
+                        <dd className="order-1 text-5xl font-extrabold text-xrpl-ws-blue">
+                            { Object.keys(this.state.preventedScam.scams).length.toLocaleString() }
+                        </dd>
+                    </div>
+                    <div className="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
+                        <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
+                            Total Transactions
+                        </dt>
+                        <dd className="order-1 text-5xl font-extrabold text-xrpl-ws-blue">
+                            { this.state.preventedScam.count.toLocaleString() }
+                        </dd>
+                    </div>
+                    <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
+                        <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
+                            Total XRP
+                        </dt>
+                        <dd className="order-1 text-5xl font-extrabold text-xrpl-ws-blue">
+                            { this.state.preventedScam.xrp.toLocaleString() }
+                        </dd>
+                    </div>
+                </dl>
 
                 <div className="text-4xl font-bold text-center text-gray-900 mt-16">Missing Destination Tag</div>
                 <div className="text-base mt-6 px-2 text-center text-gray-800">
